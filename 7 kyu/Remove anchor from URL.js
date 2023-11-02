d420fda05d323fee20d93c2531b7d028aa486cd6
Olimp <steamskv@gmail.com>
2023-11-02 05:13:37 +0200

    add

--- 7 kyu/Remove anchor from URL.js
+++ 7 kyu/Remove anchor from URL.js
@@ -3,10 +3,19 @@
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
 
-function removeUrlAnchor(url) {
-
+function removeUrlAnchor1(url) {
+    const array = url.split('')
+    const index = array.indexOf('#')
+    if (index !== -1) {
+        array.splice(index)
+    }
+    return array.join('')
 }
 
-console.log(removeUrlAnchor('www.codewars.com#about'));
-console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'));
+function removeUrlAnchor(url) {
+    return url.split('#')[0]
+}
+
+//console.log(removeUrlAnchor('www.codewars.com#about'));
+// console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'));
 console.log(removeUrlAnchor('www.codewars.com/katas/'));

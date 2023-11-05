0fc83dbbbd95df2ddcf4853b3d9dbca05d9c4a1d
Olimp <steamskv@gmail.com>
2023-11-05 14:21:15 +0200

    add

---
+++ 7 kyu/isEven - Bitwise Series.js
@@ -0,0 +1,14 @@
+// Четное ли это число?
+// Если числа четные, возврат true. Если это странно, верните false.
+
+// Ах да... следующие символы/команды отключены!
+// - использование %
+// - использование .even? в Ruby
+// - использование mod в Python
+
+var isEven1 = function (n) {
+    return Number.isInteger(n / 2) ? true : false
+}
+
+// console.log(isEven(3));
+// console.log(isEven(14));

c0968cf12f26b539f7d1dd9abc17050c8dbcf823
Olimp <steamskv@gmail.com>
2023-11-18 01:45:50 +0200

    add

--- 7 kyu/Combine objects.js
+++ 7 kyu/Combine objects.js
@@ -15,9 +15,13 @@ const objB = { a: 3, c: 6, d: 3 }
 const objC = { a: 5, d: 11, e: 8 }
 const objD = { c: 3 }
 
-function combine(obj) {
-    const result = Object.assign({}, obj)
+function combine(...obj) {
     return result
 }
-
 console.log(combine(objA, objB));
+// a = 10
+// b = 20
+// c = 30
+// a = 3
+// c = 6
+// d = 3

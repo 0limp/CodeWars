b19135105e4aa69b0fe8009a4d2e6b0b2d2495a9
Olimp <steamskv@gmail.com>
2023-11-26 18:37:18 +0200

    add

--- 7 kyu/Mobile operator detector.js
+++ 7 kyu/Mobile operator detector.js
@@ -1,29 +1,29 @@
-// Когда воин хочет поговорить с другим о мире или войне, он использует смартфон. В одной отдельной стране воинам, которые все
-// время занимались ката, не всегда хватало денег. Поэтому, если они звонят на какой-то номер, они хотят знать, какой оператор
-// обслуживает этот номер.
-
 // Напишите функцию, которая принимает число и возвращает имя оператора или строку «нет информации», если оператор не может
 // быть определенный. число всегда выглядит как 8yyyxxxxxxx, где yyy соответствует оператору.
 
-// Вот краткий список операторов:
-// 039 ххх хх хх - Голден Телеком
-// 050 ххх хх хх - МТС
-// 063 ххх хх хх - Life:)
-// 066 ххх хх хх - МТС
-// 067 ххх хх хх - Киевстар
-// 068 ххх хх хх - Билайн
-// 093 ххх хх хх - Life:)
-// 095 ххх хх хх - МТС
-// 096 ххх хх хх - Киевстар
-// 097 ххх хх хх - Киевстар
-// 098 ххх хх хх - Киевстар
-// 099 xxx xxx xx - Тест МТС [Просто верните «МТС»]
-
 function detectOperator(a) {
-    // const code = a.toString().split('').slice(1, 4).join('')
-    // const mask = a.toString().match(/ /g)
-    // return code
+    const mobileOperators = {
+        '039': 'Golden Telecom',
+        '050': "MTS",
+        '063': "Life:)",
+        '066': "MTS",
+        '067': "Kyivstar",
+        '068': "Beeline",
+        '093': "Life:)",
+        '096': "Kyivstar",
+        '097': "Kyivstar",
+        '098': "Kyivstar",
+        '099': "MTS"
+    }
+    const phoneNumber = String(a).match(/^\d{11}$/)
+    if (phoneNumber !== null) {
+        const phoneCode = phoneNumber[0].substring(1, 4);
+        if (mobileOperators.hasOwnProperty(phoneCode)) {
+            return mobileOperators[phoneCode]
+        } else {
+            return "no info"
+        }
+    } else {
+        return "no info"
+    }
 }
-
-console.log(detectOperator(80931551119))        // "Life:)
-console.log(detectOperator(8011155117711))      // "no info

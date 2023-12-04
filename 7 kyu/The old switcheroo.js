88f5fbddab17893912fb42458952db42760150e9
Olimp <steamskv@gmail.com>
2023-12-04 02:54:36 +0200

    add

---
+++ 7 kyu/The old switcheroo.js
@@ -0,0 +1,16 @@
+// Напишите функцию vowel2index(str) который принимает строку и заменяет все гласные [a,e,i,o,u] соответствующими позициями в этой строке.
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+//  == ''
+// Ваша функция должна быть нечувствительна к регистру гласных.
+
+function vowel2index(str) {
+    return str.replace(/[aeiou]+/gi, '#')
+}
+
+
+// console.log(vowel2index('this is my string'));
+// console.log(vowel2index('Codewars is the best site in the world'));
+// console.log(vowel2index('Tomorrow is going to be raining'));
+// console.log(vowel2index(''));

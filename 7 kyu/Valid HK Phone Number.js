3666dffe88ba9d55535bc75f784814676bf9c798
Olimp <steamskv@gmail.com>
2023-12-30 19:07:18 +0200

    fix

--- 7 kyu/Valid HK Phone Number.js
+++ 7 kyu/Valid HK Phone Number.js
@@ -5,14 +5,3 @@ function isValidHKPhoneNumber(str) {
 function hasValidHKPhoneNumber(str) {
     return /\d{4}\s\d{4}/.test(str.match(/\d{4} \d{4}/))
 }
-
-console.log(isValidHKPhoneNumber("1234 5678"));
-console.log(isValidHKPhoneNumber("1234 5678"));
-
-console.log(isValidHKPhoneNumber("85748475"));
-console.log(isValidHKPhoneNumber("3857  4756"));
-console.log(isValidHKPhoneNumber("sklfjsdklfjsf"));
-console.log(isValidHKPhoneNumber("     1234 5678   "));
-
-
-console.log(hasValidHKPhoneNumber("Hello, my phone number is 1234 5678"));

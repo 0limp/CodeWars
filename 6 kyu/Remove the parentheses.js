a0f7ca40a9adc845bfc50324837e0a87e3b42399
Olimp <steamskv@gmail.com>
2024-01-08 18:42:19 +0200

    add

---
+++ 6 kyu/Remove the parentheses.js
@@ -0,0 +1,24 @@
+// Удалите круглые скобки
+// В этой ката вам дана строка, например:
+// "example(unwanted thing)example"
+
+// Ваша задача — удалить все, что находится внутри скобок, а также сами скобки.
+// Пример выше вернет:
+// "exampleexample"
+
+
+// Примечания
+// Кроме круглых скобок в строке могут встречаться только буквы и пробелы. Не беспокойтесь о других скобках,
+// таких как "[]" и "{}", поскольку они никогда не появятся.
+// Круглых скобок может быть несколько.
+// Круглые скобки могут быть вложенными.
+
+function removeParentheses(s) {
+    return s.replace(/\(.+?\)/g, '')
+}
+
+console.log(removeParentheses("example(unwanted thing)example"));                           // "exampleexample"
+console.log(removeParentheses("a (bc d)e"));                                                // "a e"    
+console.log(removeParentheses("a(b(c))"));                                                  // "a"
+console.log(removeParentheses("hello example (words(more words) here) something"));         // "hello example  something"
+console.log(removeParentheses("a(first group) (second group) (third group)b"));             // "  "

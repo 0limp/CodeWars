4bbf357a6a0d76c32c4426b77357adab9c4b9a6a
Olimp <steamskv@gmail.com>
2024-02-15 22:54:21 +0200

    add

---
+++ 7 kyu/Basic Math (Add or Subtract).js
@@ -0,0 +1,17 @@
+function calculate(str) {
+    const array = str.split(/(plus|minus)/)
+    let result = parseInt(array[0])
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] === 'plus') {
+            result = result + parseInt(array[index + 1])
+        } else if (array[index] === 'minus') {
+            result = result - parseInt(array[index + 1])
+        }
+    }
+    return result + '';
+}
+
+console.log(calculate("1plus2plus3plus4"));
+console.log(calculate('1plus2plus3minus4'));
+console.log(calculate('1minus2minus3minus4'));
+
